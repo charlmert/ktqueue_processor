c7b30c3587f39aa9eef410f64acabafbfd885962
Charl Joseph Mert <charl@knowledgetree.com>
2010-04-15 17:17:46 +0200

    Create stand-alone metadata insertion jar file
    PT:2963791
    
    Updates to runMetadata.php included some more test params
    
    Committed By: Charl Joseph Mert

--- processors/runMetadata.php
+++ processors/runMetadata.php
@@ -16,9 +16,9 @@ $params = Array (
     'src_file' => '/var/www/qa_test_data/2003_doc.doc',
     'dest_file' => '/var/www/qa_test_data/2003_doc_I_New_Dem.doc',
 	'metadata' => Array ('DOC_ID' => 'DOC_ID',
-                         'DESCRIPTION' => 'Once there was a far blue boy.',
-						 'COMEDEY' => 'He didn\'t have a yonder but he was kak hungry',
-						 'FOOD' => 'Please sir can I have some more'
+                         'META1' => 'Text1',
+						 'META2' => 'Text2',
+						 'META3' => 'Text3'
                   )
 );
 
